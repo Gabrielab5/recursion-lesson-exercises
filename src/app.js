d31e9aa46a26ab971ef84aa0d2f1040abfc473e8
Gabriela <gabrielab5000@gmail.com>
2025-07-10 15:54:58 +0300

    done

--- src/app.js
+++ src/app.js
@@ -3,26 +3,36 @@
   Please note: You must also add the correct arguments to the methods
 */
 
-//Exercise 1
-const findFactorial = function() {
-  //Your code here
+const { fallback_application_name } = require("pg/lib/defaults");
 
+//Exercise 1
+const findFactorial = function(num) {
+  
+  if (num==0 || num==1) return 1;
+  return num*findFactorial(num-1)
 }
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
+const reverseString = function(str) {
+  if (str.length <= 1) return str;
+  return reverseString(str.slice(1)) + str[0];
+};
 
-}
 
 //Exercise 3
 const arr1 = [1, 2, 3]
 const arr2 = []
 
-const swap = function() {
-  //Your code here
-
+const swap = function(arr1,arr2) {
+  if (arr1.length==0) return arr2;
+  arr2.push(arr1.shift())
+  return swap(arr1,arr2)
 }
+
+console.log(findFactorial(8))
+console.log(reverseString("hello",5))
+swap(arr1,arr2)
+console.log("arr1: ["+ arr1 +"] arr2: [" + arr2+"]")
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
 module.exports = { findFactorial, reverseString, swap }
